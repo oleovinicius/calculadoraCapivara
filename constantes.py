c5e9f48fee5aefd8f04ad8090338b4b236c231d9
Leonardo V. Silva <oleovinicius@gmail.com>
2018-10-20 22:10:30 -0300

    Usando algoritmo de Chudnovsky para calculo de PI

--- constantes.py
+++ constantes.py
@@ -1,3 +1,8 @@
+from math import factorial
+from decimal import Decimal, getcontext
+
+getcontext().prec=100
+
 def constanteMenu():
     sair = False
     while sair == False:
@@ -12,9 +17,10 @@ def constanteMenu():
             print(" n")
             print(" Σ = 8/(((4*k)+1)*((4*k)+3))")
             print(" k=0")
-            passos = int(input("Digite o valor de N:"))+1
             k = int(input("Digite o valor de K:"))
-            constantePi(passos, k)
+
+            print("Valor de PI:"+ str(ConstPI(k)))
+            #constantePi(passos, k)
 
         elif opc == 2:
             print("\nPara calcular o valor Euler(e) utiliza-se:")
@@ -26,15 +32,29 @@ def constanteMenu():
         else:print("OPÇÃO INVALIDA !!!")
         pass
 
-def constantePi(passos, k):
-    valorPi =3.00
-    for k in range(k, passos):
-        j = 4*k
-        valorPi= float(valorPi + (8/((j+1)*(j+3))))
-        pass
-    return print("Valor de PI:" + str(valorPi))
+# def constantePi(passos, k):
+#     valorPi =3.00
+#     for k in range(k, passos):
+#         j = 4*k
+#         valorPi= float(valorPi + (8/((j+1)*(j+3))))
+#         pass
+#     return print("Valor de PI:" + str(valorPi))
 
 def constanteEuler(constante):
     euler = 0.00
     euler = (1+(1/constante))**constante
     return print("Resultado Euler:" + str(euler))
+
+def ConstPI(n):
+	t= Decimal(0)
+	pi = Decimal(0)
+	deno= Decimal(0)
+	k = 0
+	for k in range(n):
+		t = ((-1)**k)*(factorial(6*k))*(13591409+545140134*k)
+		deno = factorial(3*k)*(factorial(k)**3)*(640320**(3*k))
+		pi += Decimal(t)/Decimal(deno)
+	#pi = pi * Decimal(12)/Decimal(640320**(1.5))
+	pi = pi * Decimal(12)/Decimal(640320**Decimal(1.5))
+	pi = 1/pi
+	return pi
