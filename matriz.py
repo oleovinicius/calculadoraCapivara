64025f985223ababb28e9414d75d1f4ac145d992
Leonardo V. Silva <oleovinicius@gmail.com>
2018-10-28 17:09:52 -0300

    Fim da função e tratamento de erros

--- matriz.py
+++ matriz.py
@@ -10,37 +10,51 @@ def matrizMenu():
         print("4.3 Produto")
         print("4.4 Determinante")
         print("4.0 Menu principal")
-        opc = int(input("DIGITE SUA OPÇÃO:"))
+        try:
+            opc = int(input("DIGITE SUA OPÇÃO:"))
 
-        if opc == 1:
-            mlinhas = int(input("Digite o numero de linhas:"))
-            ncolunas = int(input("Digite o numero de colunas:"))
-            aux = somaMatriz(mlinhas, ncolunas)
-            print("Soma das matrizes:")
-            print(aux)
+            if opc == 1:
+                try:
+                    mlinhas = int(input("Digite o numero de linhas:"))
+                    ncolunas = int(input("Digite o numero de colunas:"))
+                    aux = somaMatriz(mlinhas, ncolunas)
+                    print("Soma das matrizes:")
+                    print(aux)
+                except:
+                    print("Parametrização errada")
+            if opc == 2:
+                try:
+                    mlinhas = int(input("Digite o numero de linhas:"))
+                    ncolunas = int(input("Digite o numero de colunas:"))
+                    aux = subMatriz(mlinhas, ncolunas)
+                    print("Subtração das matrizes:")
+                    print(aux)
+                except:
+                    print("Parametrização errada")
+            if opc == 3:
+                try:
+                    mlinhas = int(input("Digite o numero de linhas:"))
+                    ncolunas = int(input("Digite o numero de colunas:"))
+                    # aux =
+                    print("Produto das matrizes:\n", str(prodMatriz(mlinhas, ncolunas)))
+                    # print(aux)
+                except:
+                    print("Parametrização errada")
+            if opc == 4:
+                try:
+                    mlinhas = int(input("Digite o numero de linhas:"))
+                    ncolunas = int(input("Digite o numero de colunas:"))
+                    print("Determinante entre duas matrizes:\n", str(determMatriz(mlinhas,ncolunas)))
+                except:
+                    print("Parametrização errada")
 
-        if opc == 2:
-            mlinhas = int(input("Digite o numero de linhas:"))
-            ncolunas = int(input("Digite o numero de colunas:"))
-            aux = subMatriz(mlinhas, ncolunas)
-            print("Subtração das matrizes:")
-            print(aux)
 
-        if opc == 3:
-            mlinhas = int(input("Digite o numero de linhas:"))
-            ncolunas = int(input("Digite o numero de colunas:"))
-            aux = prodMatriz(mlinhas, ncolunas)
-            print("Subtração das matrizes:")
-            print(aux)
+            elif opc == 0:
+                sair = True
 
-
-
-        elif opc == 0:
-            sair = True
-        else:
-            print("\nOPÇÃO INVALIDA!!!\n")
-
-        pass
+            pass
+        except:
+            print("\nENTRADA INVALIDA\n")
 
 
 def somaMatriz(m, n):
@@ -66,28 +80,7 @@ def somaMatriz(m, n):
             # matrizB[l][c] = int(input(f"Digite o valor para [{l}, {c}]:"))
     return matrizResultado
 
-def prodMatriz(m, n):
-    matrizA = np.zeros(shape=(m, n))
-    matrizB = np.zeros(shape=(m, n))
-    matrizResultado = np.zeros(shape=(m, n))
-    for l in range(0, m):
-        for c in range(0, n):
-            matrizA[l][c] = int(input(f"Digite o valor para [{l}, {c}]:"))
 
-    print("matrizA:")
-    print(matrizA)
-    for l in range(0, m):
-        for c in range(0, n):
-            matrizB[l][c] = int(input(f"Digite o valor para [{l}, {c}]:"))
-
-    print("matrizB:")
-    print(matrizB)
-    # Subtraindo Matrizes
-    for l in range(0, m):
-        for c in range(0, n):
-            matrizResultado[l][c] = matrizA[l][c] * matrizB[c][l]
-            # matrizB[l][c] = int(input(f"Digite o valor para [{l}, {c}]:"))
-    return matrizResultado
 
 def subMatriz(m, n):
     matrizA = np.zeros(shape=(m, n))
@@ -110,4 +103,42 @@ def subMatriz(m, n):
         for c in range(0, n):
             matrizResultado[l][c] = matrizA[l][c] - matrizB[l][c]
             # matrizB[l][c] = int(input(f"Digite o valor para [{l}, {c}]:"))
+    return matrizResultado
+
+def prodMatriz(m, n):
+    matrizA = np.zeros(shape=(m, n))
+    matrizB = np.zeros(shape=(m, n))
+    matrizResultado = np.zeros(shape=(m, n))
+    for l in range(0, m):
+        for c in range(0, n):
+            matrizA[l][c] = int(input(f"Digite o valor para [{l}, {c}]:"))
+
+    print("matrizA:")
+    print(matrizA)
+    for l in range(0, m):
+        for c in range(0, n):
+            matrizB[l][c] = int(input(f"Digite o valor para [{l}, {c}]:"))
+    print("matrizB:")
+    print(matrizB)
+    # Fazendo o produto das Matrizes
+    matrizResultado = matrizA.dot(matrizB)
+    return matrizResultado
+
+def determMatriz(m, n):
+    matrizA = np.zeros(shape=(m, n))
+    matrizB = np.zeros(shape=(m, n))
+    matrizResultado = np.zeros(shape=(m, n))
+    for l in range(0, m):
+        for c in range(0, n):
+            matrizA[l][c] = int(input(f"Digite o valor para [{l}, {c}]:"))
+
+    print("matrizA:")
+    print(matrizA)
+    for l in range(0, m):
+        for c in range(0, n):
+            matrizB[l][c] = int(input(f"Digite o valor para [{l}, {c}]:"))
+    print("matrizB:")
+    print(matrizB)
+    # Fazendo a determinante das Matrizes
+    matrizResultado = np.matmul(matrizA,matrizB)
     return matrizResultado
