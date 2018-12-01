dc3664e8ed9814b6e99031710107441300c572f4
Leonardo V. Silva <oleovinicius@gmail.com>
2018-11-30 23:12:54 -0200

    Instalador Windows Funcionando Funcionando

--- instalador.bat
+++ instalador.bat
@@ -1,20 +1,27 @@
 goto MAIN
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 :MAIN
+echo off
 cls
-echo "BEM VINDO"
-echo "Iniciando instalação de pacotes"
+echo BEM VINDO
+echo Iniciando instalacao de pacotes
+echo Inicianto metodo1 (python para um usuario)
+pause
+goto metodo1
+
+:metodo1
+cd C:\Users\%username%\AppData\Local\Programs\Python\Python37-32
+python -m pip install --upgrade pip
+pip install -r C:\Users\%username%\Documents\calculadoraCapivara\requirements.txt
+pip install -r C:\Users\%username%\Documentos\calculadoraCapivara\requirements.txt
+echo Iniciando metodo2 (python para todos os usuarios)
+goto metodo2
+
+:metodo2
+cd C:\Program Files\Python37
+pause
+python -m pip install --upgrade pip
+pause
+pip install -r C:\Users\%username%\Documents\calculadoraCapivara\requirements.txt
+pause
+pip install -r C:\Users\%username%\Documentos\calculadoraCapivara\requirements.txt
